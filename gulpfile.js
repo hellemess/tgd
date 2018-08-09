d5f38502b89d1e9ed6d0c4aead62da87b9edc1ea
Elena Esina <hellemess@yandex.ru>
2018-08-09 18:43:54 +0300

    унифицирует отступы

--- gulpfile.js
+++ gulpfile.js
@@ -14,91 +14,91 @@ const run = require('run-sequence');
 // html
 
 gulp.task('html', () => {
-	return gulp.src('source/*.html')
-		.pipe(gulp.dest('build'))
-		.pipe(server.stream());
+  return gulp.src('source/*.html')
+    .pipe(gulp.dest('build'))
+    .pipe(server.stream());
 });
 
 // less
 
 gulp.task('less', () => {
-	return gulp.src('source/less/style.less')
-		.pipe(plumber())
-		.pipe(less())
-		.pipe(postcss([
-			postcssPresetEnv({
-				stage: 4
-			}),
-			autoprefixer
-		]))
-		.pipe(csso())
-		.pipe(gulp.dest('build/css'))
-		.pipe(server.stream());
+  return gulp.src('source/less/style.less')
+    .pipe(plumber())
+    .pipe(less())
+    .pipe(postcss([
+      postcssPresetEnv({
+        stage: 4
+      }),
+      autoprefixer
+    ]))
+    .pipe(csso())
+    .pipe(gulp.dest('build/css'))
+    .pipe(server.stream());
 });
 
 // fonts
 
 gulp.task('fonts', () => {
-	return gulp.src('source/fonts/*.woff')
-		.pipe(gulp.dest('build/fonts'))
-		.pipe(server.stream());
+  return gulp.src('source/fonts/*.woff')
+    .pipe(gulp.dest('build/fonts'))
+    .pipe(server.stream());
 });
 
 // images
 
 gulp.task('images', () => {
-	return gulp.src('source/images/**/*.{gif,jpg,png,svg}')
-	.pipe(imagemin([
-		imagemin.gifsicle({interlaced: true}),
-		imagemin.jpegtran({progressive: true}),
-		imagemin.optipng({optimizationLevel: 3}),
-		imagemin.svgo()
-	]))
-	.pipe(gulp.dest('build/images'))
-	.pipe(server.stream());
+  return gulp.src('source/images/**/*.{gif,jpg,png,svg}')
+  .pipe(imagemin([
+    imagemin.gifsicle({interlaced: true}),
+    imagemin.jpegtran({progressive: true}),
+    imagemin.optipng({optimizationLevel: 3}),
+    imagemin.svgo()
+  ]))
+  .pipe(gulp.dest('build/images'))
+  .pipe(server.stream());
 });
 
 // js
 
 gulp.task('js', () => {
-	return gulp.src('source/js/script.js')
-		.pipe(jsmin())
-		.pipe(gulp.dest('build/js'))
-		.pipe(server.stream());
+  return gulp.src('source/js/script.js')
+    .pipe(jsmin())
+    .pipe(gulp.dest('build/js'))
+    .pipe(server.stream());
 });
 
 // build
 
 gulp.task('clear', () => {
-	return del('build');
+  return del('build');
 });
 
 gulp.task('build', () => {
-	run(
-		'clear',
-		'js',
-		'images',
-		'fonts',
-		'less',
-		'html'
-	);
+  run(
+    'clear',
+    'js',
+    'images',
+    'fonts',
+    'less',
+    'html'
+  );
 });
 
 // watch
 
 gulp.task('update', ['html'], () => {
-	server.reload();
+  server.reload();
 })
 
 gulp.task('watch', () => {
-	server.init({
-		notify: false,
-		server: 'build/'
-	});
+  server.init({
+    notify: false,
+    server: 'build/'
+  });
 
-	gulp.watch('source/js/script.js', ['js']);
-	gulp.watch('source/images/*', ['images']);
-	gulp.watch('source/fonts/*', ['fonts']);
-	gulp.watch('source/less/**/*.less', ['less']);
-	gulp.watch('source/*.html', ['update']);
+  gulp.watch('source/js/script.js', ['js']);
+  gulp.watch('source/images/*', ['images']);
+  gulp.watch('source/fonts/*', ['fonts']);
+  gulp.watch('source/less/**/*.less', ['less']);
+  gulp.watch('source/*.html', ['update']);
 });
--- source/js/script.js
+++ source/js/script.js
@@ -1,9 +1,9 @@
 (() => {
-	const ENTER = 13;
+  const ENTER = 13;
 
-	const isActivationEvent = (evt) => evt.keyCode && evt.keyCode === ENTER;
+  const isActivationEvent = (evt) => evt.keyCode && evt.keyCode === ENTER;
 
-	const addHandler = (element, action) => {
+  const addHandler = (element, action) => {
     element.addEventListener(`click`, action);
 
     element.addEventListener(`keydown`, (evt) => {
@@ -13,164 +13,164 @@
     });
   };
 
-	const enableSwitch = (element) => {
-		const disableBtns = (itemsCount, clickCount) => {
-			element.prevBtn.disabled = clickCount === 0 ? true : false;
+  const enableSwitch = (element) => {
+    const disableBtns = (itemsCount, clickCount) => {
+      element.prevBtn.disabled = clickCount === 0 ? true : false;
 
-			if (window.innerWidth < 768 || element.oneItem) {
-				element.nextBtn.disabled = itemsCount - 1 > clickCount ? false : true;
-			} else if (window.innerWidth < 1024) {
-				element.nextBtn.disabled = itemsCount - 2 > clickCount ? false : true;
-			} else if (element.condensed && window.innerWidth < 1200) {
-				element.nextBtn.disabled = itemsCount - 3 > clickCount ? false : true;
-			} else if (element.condensed) {
-				element.nextBtn.disabled = itemsCount - 4 > clickCount ? false : true;
-			} else {
-				element.nextBtn.disabled = itemsCount - 2 > clickCount ? false : true;
-			}
-		};
+      if (window.innerWidth < 768 || element.oneItem) {
+        element.nextBtn.disabled = itemsCount - 1 > clickCount ? false : true;
+      } else if (window.innerWidth < 1024) {
+        element.nextBtn.disabled = itemsCount - 2 > clickCount ? false : true;
+      } else if (element.condensed && window.innerWidth < 1200) {
+        element.nextBtn.disabled = itemsCount - 3 > clickCount ? false : true;
+      } else if (element.condensed) {
+        element.nextBtn.disabled = itemsCount - 4 > clickCount ? false : true;
+      } else {
+        element.nextBtn.disabled = itemsCount - 2 > clickCount ? false : true;
+      }
+    };
 
-		let itemsCount = element.content.children.length;
+    let itemsCount = element.content.children.length;
 
-		if (element.content.children[0].classList.contains(`schedule__item--common`)) {
-			if (window.innerWidth < 768) {
-				itemsCount += 0;
-			} else if (window.innerWidth < 1024) {
-				itemsCount += 1;
-			} else if (window.innerWidth < 1200) {
-				itemsCount += 2;
-			} else {
-				itemsCount += 3;
-			}
-		}
+    if (element.content.children[0].classList.contains(`schedule__item--common`)) {
+      if (window.innerWidth < 768) {
+        itemsCount += 0;
+      } else if (window.innerWidth < 1024) {
+        itemsCount += 1;
+      } else if (window.innerWidth < 1200) {
+        itemsCount += 2;
+      } else {
+        itemsCount += 3;
+      }
+    }
 
-		let currentPosition = 0;
-		let clickCount = 0;
+    let currentPosition = 0;
+    let clickCount = 0;
 
-		disableBtns(itemsCount, clickCount);
+    disableBtns(itemsCount, clickCount);
 
-		const switchItems = (evt) => {
-			if (!evt.target.disabled && evt.target === element.prevBtn) {
-				clickCount -= 1;
+    const switchItems = (evt) => {
+      if (!evt.target.disabled && evt.target === element.prevBtn) {
+        clickCount -= 1;
 
-				if (window.innerWidth < 768 || element.oneItem) {
-					currentPosition += 100;
-				} else if (window.innerWidth < 1024) {
-					currentPosition += 50;
-				} else if (element.condensed && window.innerWidth < 1200) {
-					currentPosition += 33.3333333;
-				} else if (element.condensed) {
-					currentPosition += 25;
-				} else {
-					currentPosition += 50;
-				}
-			} else if (!evt.target.disabled && evt.target === element.nextBtn) {
-				clickCount += 1;
+        if (window.innerWidth < 768 || element.oneItem) {
+          currentPosition += 100;
+        } else if (window.innerWidth < 1024) {
+          currentPosition += 50;
+        } else if (element.condensed && window.innerWidth < 1200) {
+          currentPosition += 33.3333333;
+        } else if (element.condensed) {
+          currentPosition += 25;
+        } else {
+          currentPosition += 50;
+        }
+      } else if (!evt.target.disabled && evt.target === element.nextBtn) {
+        clickCount += 1;
 
-				if (window.innerWidth < 768 || element.oneItem) {
-					currentPosition -= 100;
-				} else if (window.innerWidth < 1024) {
-					currentPosition -= 50;
-				} else if (element.condensed && window.innerWidth < 1200) {
-					currentPosition -= 33.3333333;
-				} else if (element.condensed) {
-					currentPosition -= 25;
-				} else {
-					currentPosition -= 50;
-				}
-			}
+        if (window.innerWidth < 768 || element.oneItem) {
+          currentPosition -= 100;
+        } else if (window.innerWidth < 1024) {
+          currentPosition -= 50;
+        } else if (element.condensed && window.innerWidth < 1200) {
+          currentPosition -= 33.3333333;
+        } else if (element.condensed) {
+          currentPosition -= 25;
+        } else {
+          currentPosition -= 50;
+        }
+      }
 
-			element.content.style.left = `${currentPosition}%`;
-			disableBtns(itemsCount, clickCount);
-	  }
+      element.content.style.left = `${currentPosition}%`;
+      disableBtns(itemsCount, clickCount);
+    }
 
-		addHandler(element.frame, switchItems);
-	}
+    addHandler(element.frame, switchItems);
+  }
 
-	const speakers = {
-		frame: document.querySelector(`.speakers__frame`),
-		prevBtn: document.querySelector(`.speakers__btn--prev`),
-		nextBtn: document.querySelector(`.speakers__btn--next`),
-		content: document.querySelector(`.speakers__list`)
-	};
+  const speakers = {
+    frame: document.querySelector(`.speakers__frame`),
+    prevBtn: document.querySelector(`.speakers__btn--prev`),
+    nextBtn: document.querySelector(`.speakers__btn--next`),
+    content: document.querySelector(`.speakers__list`)
+  };
 
-	const removeNoticeBtn = () => {
-		speakers.nextBtn.classList.remove(`speakers__btn--notice`);
-	}
+  const removeNoticeBtn = () => {
+    speakers.nextBtn.classList.remove(`speakers__btn--notice`);
+  }
 
-	addHandler(speakers.nextBtn, removeNoticeBtn);
-	enableSwitch(speakers);
+  addHandler(speakers.nextBtn, removeNoticeBtn);
+  enableSwitch(speakers);
 
-	const rooms = document.querySelectorAll(`.schedule__frame`);
-	const btns = document.querySelectorAll(`.schedule__btn`);
-	const noticeSwitchBtn = btns[1];
+  const rooms = document.querySelectorAll(`.schedule__frame`);
+  const btns = document.querySelectorAll(`.schedule__btn`);
+  const noticeSwitchBtn = btns[1];
 
-	const removeNoticeSwitch = () => {
-		noticeSwitchBtn.classList.remove(`schedule__btn--notice`);
-	}
+  const removeNoticeSwitch = () => {
+    noticeSwitchBtn.classList.remove(`schedule__btn--notice`);
+  }
 
-	Array.from(btns).forEach((it) => {
-		addHandler(it, removeNoticeSwitch);
-	});
+  Array.from(btns).forEach((it) => {
+    addHandler(it, removeNoticeSwitch);
+  });
 
-	Array.from(rooms).forEach((it) => {
-		const room = {
-			frame: it,
-			prevBtn: it.querySelector(`.schedule__btn--prev`),
-			nextBtn: it.querySelector(`.schedule__btn--next`),
-			content: it.querySelector(`.schedule__list`),
-			condensed: true
-		};
+  Array.from(rooms).forEach((it) => {
+    const room = {
+      frame: it,
+      prevBtn: it.querySelector(`.schedule__btn--prev`),
+      nextBtn: it.querySelector(`.schedule__btn--next`),
+      content: it.querySelector(`.schedule__list`),
+      condensed: true
+    };
 
-		enableSwitch(room);
-	});
+    enableSwitch(room);
+  });
 
-	const gallery = {
-		frame: document.querySelector(`.gallery__frame`),
-		prevBtn: document.querySelector(`.gallery__btn--prev`),
-		nextBtn: document.querySelector(`.gallery__btn--next`),
-		content: document.querySelector(`.gallery__list`),
-		oneItem: true
-	};
+  const gallery = {
+    frame: document.querySelector(`.gallery__frame`),
+    prevBtn: document.querySelector(`.gallery__btn--prev`),
+    nextBtn: document.querySelector(`.gallery__btn--next`),
+    content: document.querySelector(`.gallery__list`),
+    oneItem: true
+  };
 
-	if (window.innerWidth < 1200) {
-		const removeNoticeGallery = () => {
-			gallery.nextBtn.classList.remove(`gallery__btn--notice`);
-		}
+  if (window.innerWidth < 1200) {
+    const removeNoticeGallery = () => {
+      gallery.nextBtn.classList.remove(`gallery__btn--notice`);
+    }
 
-		addHandler(gallery.nextBtn, removeNoticeGallery);
+    addHandler(gallery.nextBtn, removeNoticeGallery);
 
-		enableSwitch(gallery);
-	} else {
-		const pictures = Array.from(gallery.content.querySelectorAll(`.gallery__item`));
+    enableSwitch(gallery);
+  } else {
+    const pictures = Array.from(gallery.content.querySelectorAll(`.gallery__item`));
 
 
-		Array.from(pictures).forEach((it) => {
-			const element = gallery.content.removeChild(it);
-		});
+    Array.from(pictures).forEach((it) => {
+      const element = gallery.content.removeChild(it);
+    });
 
-		const moreBtn = document.querySelector(`.gallery__more`);
+    const moreBtn = document.querySelector(`.gallery__more`);
 
-		const addPictures = () => {
-			for (let i = 0; i < 3; i++) {
-				if (pictures.length > 1) {
-					gallery.content.appendChild(pictures[i]);
-					pictures.shift();
-				} else {
-					moreBtn.style.display = `none`;
-				}
-			}
-		}
+    const addPictures = () => {
+      for (let i = 0; i < 3; i++) {
+        if (pictures.length > 1) {
+          gallery.content.appendChild(pictures[i]);
+          pictures.shift();
+        } else {
+          moreBtn.style.display = `none`;
+        }
+      }
+    }
 
-		addPictures();
+    addPictures();
 
-		addHandler(moreBtn, addPictures);
-	}
+    addHandler(moreBtn, addPictures);
+  }
 
-	const header = document.querySelector(`header`);
-	const backBtn = document.querySelector(`.back`);
+  const header = document.querySelector(`header`);
+  const backBtn = document.querySelector(`.back`);
 
-	window.addEventListener(`scroll`, (evt) => {
-		backBtn.style.display = header.getBoundingClientRect().bottom < 0 ? `block` : `none`;
-	})
+  window.addEventListener(`scroll`, (evt) => {
+    backBtn.style.display = header.getBoundingClientRect().bottom < 0 ? `block` : `none`;
+  })
 })();
